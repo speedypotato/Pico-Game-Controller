c40a4355608c1188b355de7a01650b4ce6599571
Nicholas Gadjali <speeedypotato@gmail.com>
2021-02-15 09:37:12 -0800

    Working encoder implementation. Thanks Drewol & Zoids

--- src/pico_sdvx.c
+++ src/pico_sdvx.c
@@ -2,7 +2,7 @@
  * Pico SDVX
  * @author SpeedyPotato
  * 
- * Based off dev_hid_composite
+ * Based off dev_hid_composite and PaulStoffregen/Encoder
  */
 
 #include <stdlib.h>
@@ -15,7 +15,7 @@
 
 #include "usb_descriptors.h"
 
-const uint8_t ENC_SENS = 30;        // Encoder sensitivity multiplier
+const uint8_t ENC_SENS = 10;        // Encoder sensitivity multiplier
 const uint8_t L_ENC_GPIO[] = {0, 1};
 const uint8_t R_ENC_GPIO[] = {2, 3};
 const uint8_t SW_KEYCODE[] = {HID_KEY_RETURN, HID_KEY_A, HID_KEY_S, HID_KEY_D, HID_KEY_F, HID_KEY_Z, HID_KEY_X};    // MODIFY KEYBINDS HERE
@@ -25,21 +25,74 @@ const size_t ENC_GPIO_SIZE = sizeof(L_ENC_GPIO)/ sizeof(L_ENC_GPIO[0]);
 const size_t SW_GPIO_SIZE = sizeof(SW_GPIO)/ sizeof(SW_GPIO[0]);
 const size_t LED_GPIO_SIZE = sizeof(LED_GPIO)/ sizeof(LED_GPIO[0]);
 
-int l_enc=0, r_enc=0;
-int l_state, l_prev_state, r_state, r_prev_state;
+static const char *gpio_irq_str[] = {
+        "LEVEL_LOW",  // 0x1
+        "LEVEL_HIGH", // 0x2
+        "EDGE_FALL",  // 0x4
+        "EDGE_RISE"   // 0x8
+};
+
+int l_enc=0, r_enc=0, l_state, r_state;
+
+/**
+ * Encoder Callback
+ **/
+void gpio_enc_callback(uint gpio, uint32_t events) {
+    if (gpio == L_ENC_GPIO[0] || gpio == L_ENC_GPIO[1]) {
+        uint8_t s = l_state & 3;
+        if (gpio_get(L_ENC_GPIO[0])) s |= 4;
+        if (gpio_get(L_ENC_GPIO[1])) s |= 8;
+        switch (s) {
+            case 0: case 5: case 10: case 15:
+                break;
+            case 1: case 7: case 8: case 14:
+                l_enc++; break;
+            case 2: case 4: case 11: case 13:
+                l_enc--; break;
+            case 3: case 12:
+                l_enc += 2; break;
+            default:
+                l_enc -= 2; break;
+        }
+        l_state = (s >> 2);
+    } else {
+        uint8_t s = r_state & 3;
+        if (gpio_get(R_ENC_GPIO[0])) s |= 4;
+        if (gpio_get(R_ENC_GPIO[1])) s |= 8;
+        switch (s) {
+            case 0: case 5: case 10: case 15:
+                break;
+            case 1: case 7: case 8: case 14:
+                r_enc++; break;
+            case 2: case 4: case 11: case 13:
+                r_enc--; break;
+            case 3: case 12:
+                r_enc += 2; break;
+            default:
+                r_enc -= 2; break;
+        }
+        r_state = (s >> 2);
+    }
+}
 
 /**
  * Initialize board pins
  **/
 void init() {
+    // LED Pin on when connected
+    gpio_init(25);
+    gpio_set_dir(25, GPIO_OUT);
+    gpio_put(25, 1);
+
     // Setup L Encoder GPIO
     for (int i = 0; i < ENC_GPIO_SIZE; i++) {
         gpio_init(L_ENC_GPIO[i]);
         gpio_set_function(L_ENC_GPIO[i], GPIO_FUNC_SIO);
         gpio_set_dir(L_ENC_GPIO[i], GPIO_IN);
         gpio_pull_up(L_ENC_GPIO[i]);
+        gpio_set_irq_enabled_with_callback(L_ENC_GPIO[i], GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_enc_callback);
     }
-    l_prev_state = gpio_get(L_ENC_GPIO[0]);
+    l_state = gpio_get(L_ENC_GPIO[0]);
 
     // Setup R Encoder GPIO
     for (int i = 0; i < ENC_GPIO_SIZE; i++) {
@@ -47,8 +100,9 @@ void init() {
         gpio_set_function(R_ENC_GPIO[i], GPIO_FUNC_SIO);
         gpio_set_dir(R_ENC_GPIO[i], GPIO_IN);
         gpio_pull_up(R_ENC_GPIO[i]);
+        gpio_set_irq_enabled_with_callback(R_ENC_GPIO[i], GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_enc_callback);
     }
-    r_prev_state = gpio_get(R_ENC_GPIO[0]);
+    r_state = gpio_get(R_ENC_GPIO[0]);
 
     // Setup Button GPIO
     for (int i = 0; i < SW_GPIO_SIZE; i++) {
@@ -63,20 +117,6 @@ void init() {
  * Keyboard Mode
  **/
 void key_mode() {
-    // Poll every 10ms
-    const uint32_t interval_ms = 10;
-    static uint32_t start_ms = 0;
-
-    if (board_millis() - start_ms < interval_ms) return; // not enough time
-    start_ms += interval_ms;
-
-    // Remote wakeup
-    // if (tud_suspended() && btn) {
-    //     // Wake up host if we are in suspend mode
-    //     // and REMOTE_WAKEUP feature is enabled by host
-    //     tud_remote_wakeup();
-    // }
-
     if (tud_hid_ready()) {
         /*------------- Keyboard -------------*/
         bool isPressed = false;
@@ -85,7 +125,6 @@ void key_mode() {
                 // use to avoid send multiple consecutive zero report for keyboard
                 uint8_t keycode[6] = {0};
                 keycode[0] = SW_KEYCODE[i];
-
                 tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, keycode);
 
                 isPressed = true;
@@ -93,29 +132,14 @@ void key_mode() {
         }
         // send empty key report if previously has key pressed
         if (!isPressed) tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, NULL);
-        
+
         // delay a bit before attempt to send keyboard report
         board_delay(10);
 
         /*------------- Mouse -------------*/
-        //todo: merge these together into less hid mouse reports, move to interrupts
-        l_state = gpio_get(L_ENC_GPIO[0]);
-        if (l_state != l_prev_state) {
-            if (gpio_get(L_ENC_GPIO[1]) != l_state) {
-                tud_hid_mouse_report(REPORT_ID_MOUSE, 0x00, 1 * ENC_SENS, 0, 0, 0);
-            } else {
-                tud_hid_mouse_report(REPORT_ID_MOUSE, 0x00, -1 * ENC_SENS, 0, 0, 0);
-            }
-        }
-
-        r_state = gpio_get(R_ENC_GPIO[0]);
-        if (r_state != r_prev_state) {
-            if (gpio_get(R_ENC_GPIO[1]) != r_state) {
-                tud_hid_mouse_report(REPORT_ID_MOUSE, 0x00, 0, 1 * ENC_SENS, 0, 0);
-            } else {
-                tud_hid_mouse_report(REPORT_ID_MOUSE, 0x00, 0, -1 * ENC_SENS, 0, 0);
-            }
-        }
+        tud_hid_mouse_report(REPORT_ID_MOUSE, 0x00, l_enc * ENC_SENS, r_enc * ENC_SENS, 0, 0);
+        l_enc = 0;
+        r_enc = 0;
     }
 }
 
