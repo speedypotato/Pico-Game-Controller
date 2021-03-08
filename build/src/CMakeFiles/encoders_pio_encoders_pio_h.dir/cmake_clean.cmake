ff86b07e2a7e63fe08c2b71e8fc2aac5ad42bc62
Nicholas Gadjali <speeedypotato@gmail.com>
2021-03-07 21:11:56 -0800

    Added encoders.pio from mdxtinkernick and fixed makefile

---
+++ build/src/CMakeFiles/encoders_pio_encoders_pio_h.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/encoders_pio_encoders_pio_h"
+  "encoders.pio.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/encoders_pio_encoders_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
