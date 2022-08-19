8253b171b2263bb7a892480b9faff983c7f6226c
Nicholas G <speeedypotato@gmail.com>
2022-08-18 21:30:16 -0400

    Renamed debounce algorithms

--- src/debounce/debounce_include.h
+++ src/debounce/debounce_include.h
@@ -1,11 +1,12 @@
 /**
  * Simple header file to include all files in the folder
  * @author SpeedyPotato
- * 
- * To add a debounce mode, return a uint16_t representing the button states.  These are saved in report.buttons as truth.
- * Create debounce mode as desired and then add the #include here.
+ *
+ * To add a debounce mode, return a uint16_t representing the button states.
+ * These are saved in report.buttons as truth. Create debounce mode as desired
+ * and then add the #include here.
  **/
 extern uint64_t sw_timestamp[SW_GPIO_SIZE];
 
-#include "constant_switch.c"
-#include "minimum_hold.c"
+#include "deferred.c"
+#include "eager.c"
---
+++ src/debounce/eager.c
@@ -0,0 +1,18 @@
+/**
+ * Debouncing algorithm which sends a report immediately when a switch is
+ * triggered and holds it for n amount of time.
+ * @author SpeedyPotato
+ **/
+
+uint16_t debounce_eager() {
+  uint16_t translate_buttons = 0;
+  for (int i = SW_GPIO_SIZE - 1; i >= 0; i--) {
+    if (time_us_64() - sw_timestamp[i] <= SW_DEBOUNCE_TIME_US ||
+        !gpio_get(SW_GPIO[i])) {
+      translate_buttons = (translate_buttons << 1) | 1;
+    } else {
+      translate_buttons <<= 1;
+    }
+  }
+  return translate_buttons;
+}
